5b28bb624d3add6896f9ab1b200b5b8fcd4fc949
tanobit <aklramewaruitano@gmail.com>
2021-07-29 22:41:44 +0900

    route.jsの変数に、constを個別に付けていたため、一つだけ付けて後は削除した

--- routes/route.js
+++ routes/route.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controller/UserController');
-const passport = require("passport");
+const express = require('express'),
+  router = express.Router(),
+  userController = require('../controller/UserController'),
+  passport = require("passport");
 
 //みんなの投稿
 router.get('/', userController.displayMono);
 
-router.post('/', userController.addMono)
+router.post('/', userController.addMono);
 
-router.get('/signup', userController.getSignUp)
+router.get('/signup', userController.getSignUp);
 
-router.post('/signup', userController.postSignUp)
+router.post('/signup', userController.postSignUp);
 
-router.get('/signin', userController.getSignIn)
+router.get('/signin', userController.getSignIn);
 
 // ログイン処理
 router.post('/signin', passport.authenticate('local',
@@ -23,11 +23,11 @@ router.post('/signin', passport.authenticate('local',
   }
 ));
 
-router.get('/logout', userController.getLogOut)
+router.get('/logout', userController.getLogOut);
 
-router.post('/delete', userController.postDelete)
+router.post('/delete', userController.postDelete);
 
-router.post('/edit', userController.postEdit)
+router.post('/edit', userController.postEdit);
 
 module.exports = router;
 

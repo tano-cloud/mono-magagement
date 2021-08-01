4d7b4082b04af85af9a69cea4145bdf5f512dee1
tanobit <aklramewaruitano@gmail.com>
2021-08-01 12:43:48 +0900

    mysql workbenchにて、新しくDBを作成して使用している為、passwordの設定が変わったためコミット

--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,7 @@ module.exports = {
     connection: {
       database: "todo_app",
       user: "root",
-      password: null,
+      password: 'password',
     },
     pool: {
       min: 2,
@@ -20,7 +20,7 @@ module.exports = {
     connection: {
       database: "todo_app",
       user: "root",
-      password: null,
+      password: 'password',
     },
     pool: {
       min: 2,
@@ -33,7 +33,7 @@ module.exports = {
     connection: {
       database: "todo_app",
       user: "root",
-      password: null,
+      password: 'password',
     },
     pool: {
       min: 2,
